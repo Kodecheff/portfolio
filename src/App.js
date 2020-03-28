f422d99b485fa5bf626c627b088b4e464dfafb72
kodecheff <akunneemeka125@gmail.com>
2020-03-29 00:42:25 +0100

    Installed gh-pages

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import Works from './Works';
 import About from './About';
 import PathError from './404';
 import './app.css';
-import { BrowserRouter, Route, Switch} from "react-router-dom";
+import { HashRouter, Route, Switch} from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
     <Switch>
       <Route exact path="/" component={Home}/>
       <Route exact path="/works" component={Works}/>
       <Route exact path="/about" component={About}/>
       <Route component={PathError}/>
     </Switch>
-    </BrowserRouter>
+    </HashRouter>
    
   );
 }
