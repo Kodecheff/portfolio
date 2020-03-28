3fc79160c86942b134f8e5d660216ec3374f5687
kodecheff <akunneemeka125@gmail.com>
2020-03-29 00:51:38 +0100

    Updates

---
+++ precache-manifest.a5a3cba2771dbc3af3cd6d9c46767939.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb82c8ae7af10a4f8a79e3eda890cda0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e2a2e5f6c1e5a436d908",
+    "url": "/portfolio/static/css/main.b7006291.chunk.css"
+  },
+  {
+    "revision": "e8628b29806022dfcb87",
+    "url": "/portfolio/static/js/2.6f08c2f8.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/portfolio/static/js/2.6f08c2f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2a2e5f6c1e5a436d908",
+    "url": "/portfolio/static/js/main.49a8adb1.chunk.js"
+  },
+  {
+    "revision": "1a95985286676740b556",
+    "url": "/portfolio/static/js/runtime-main.f6eb50f0.js"
+  },
+  {
+    "revision": "59f8d18bc656e0f8f96061c9e3745472",
+    "url": "/portfolio/static/media/about-banner.59f8d18b.png"
+  },
+  {
+    "revision": "bf3fc4348b157bd02a06af4bf1a52c5e",
+    "url": "/portfolio/static/media/banner-img.bf3fc434.png"
+  }
+]);
