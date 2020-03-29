a5a0f1b5f249c9a3ae4c3dde441a032382b32fca
kodecheff <akunneemeka125@gmail.com>
2020-03-30 00:15:38 +0100

    added effect

--- src/component/Banner.js
+++ src/component/Banner.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import Typewriter from 'typewriter-effect';
 import './main.css';
-import img from '../img/banner-img.png';
+import img from '../img/home-banner.jpg';
 
 const Banner = () => {
   return(
@@ -9,13 +10,21 @@ const Banner = () => {
     <div class="row">
       <div class="col-md-6 col-lg-6 col-sm-6 col-xs-12">
       <p class="banner content ">
-        <span className="h5">Hi,</span><br />
-        <span className="h3">I'm Pascal</span><br />
-        <span className="h1">Software Developer</span>
-      </p>
-    </div>
+        <Typewriter
+  onInit={(typewriter) => {
+    typewriter.typeString('<span class="h5">Hi,</span><br />')
+    .pauseFor(1500)
+    .typeString('<span class="h3">I\'m Pascal </span>')
+    .pauseFor(1500)
+    .typeString('<br/>')
+    .typeString('<span class="h1">Software Developer </span>')
+    .changeCursor(' ')
+    .start();
+    }}
+      />
+      </p> 
+    </div> 
     
-    {/* <div class="clearfix visible-xs"></div> */}
 
     <div class="col-md-6 col-lg-6 col-sm-6 col-offset-sm col-xs-12">
       <span class="banner banner-img hidden-xs">
