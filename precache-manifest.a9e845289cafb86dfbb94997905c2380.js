170ff3d61116acde12bf5bd5bd3c198995351aa8
kodecheff <akunneemeka125@gmail.com>
2020-03-30 00:17:33 +0100

    Updates

---
+++ precache-manifest.a9e845289cafb86dfbb94997905c2380.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b52aecfdb27d2c209b354600f30eac3e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1099832ae2f7f0824e25",
+    "url": "/portfolio/static/css/main.c8b7054b.chunk.css"
+  },
+  {
+    "revision": "9d27f16118300dbf2191",
+    "url": "/portfolio/static/js/2.690ba37b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/portfolio/static/js/2.690ba37b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1099832ae2f7f0824e25",
+    "url": "/portfolio/static/js/main.b0ddcbee.chunk.js"
+  },
+  {
+    "revision": "1a95985286676740b556",
+    "url": "/portfolio/static/js/runtime-main.f6eb50f0.js"
+  },
+  {
+    "revision": "59f8d18bc656e0f8f96061c9e3745472",
+    "url": "/portfolio/static/media/about-banner.59f8d18b.png"
+  },
+  {
+    "revision": "78f0903a82e9d51338c85aaba8fe9341",
+    "url": "/portfolio/static/media/home-banner.78f0903a.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.ac2ff4fc751eea7d1490ab463c2de819.js"
+  "/portfolio/precache-manifest.a9e845289cafb86dfbb94997905c2380.js"
 );
 
 self.addEventListener('message', (event) => {
